770dfef7675c7c2d4de439e72ea97470640fcf87
Administrator <Administrator@172.31.36.201>
2023-09-19 07:11:53 +0000

    initial commit

---
+++ src/utils/display_configuration.js
@@ -0,0 +1,48 @@
+export const petCareCenterViewConfig = {
+Details: [
+    {
+      key: "Name",
+      value: "Name",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Address",
+      value: "Address",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "BranchName",
+      value: "BranchName",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Description",
+      value: "Description",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Pincode",
+      value: "Pincode",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "IsOperational",
+      value: "IsOperational",
+      type: "boolean",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
